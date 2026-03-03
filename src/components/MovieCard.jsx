98b4ba458b8b4e1317c4ffc18f1a5a1b37051c43
KarmPandya007 <karmpandya007@gmail.com>
2026-03-03 17:20:16 +0530

    Made the proper alignment for images in both movies and series page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,18 +3,21 @@ import React, { useState } from 'react'
 const MovieCard = ({ heading }) => {
 
     const movies = [
-        { id: 1, name: "Ironman", year: 2008, description: "First Movie of Ironman" },
-        { id: 2, name: "Ironman 2", year: 2010, description: "Second Movie of Ironman" },
-        { id: 3, name: "Ironman 3", year: 2012, description: "Third Movie of Ironman" },
+        { id: 1, name: "Ironman", image: "https://m.media-amazon.com/images/I/71gGg48AKcL._AC_UF894,1000_QL80_.jpg", year: 2008, description: "First Movie of Ironman" },
+        { id: 2, name: "Ironman 2", image: "https://play-lh.googleusercontent.com/Y19Z2gHT3plZCrsbZPY9zr7Ru6nBOfLygm1jNr0NkHA93tGQz8yHB2jj4Y_Oc0W9lULBTQ", year: 2010, description: "Second Movie of Ironman" },
+        { id: 3, name: "Ironman 3", image: "https://thechristiannerd.files.wordpress.com/2013/04/iron-man-3-imax-poster.jpg", year: 2012, description: "Third Movie of Ironman" },
     ]
 
     return (
-        <div className='my-10 p-2'>
-            <h1 className='bg-blue-200'>{heading}</h1>
-            <ul>
+        <div className='p-2'>
+            <h1 className='bg-blue-200 text-center py-4 m-2'>{heading}</h1>
+            <ul className='flex flex-row'>
                 {movies.map((movie) => (
-                    <li key={movie.id}>
-                        {movie.name} ({movie.year}) - {movie.description}
+                    <li key={movie.id} className='flex flex-col gap-2 ml-10'>
+                        <img src={movie.image} height={300} width={300} alt="photo" />
+                        <li>{movie.name}</li>
+                        <li>{movie.year}</li>
+                        <li>{movie.description}</li>
                     </li>
                 ))}
             </ul>
--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 
 const SeriesCard = ({ heading }) => {
     const serieses = [
-        { id: 1, name: "Ironman", year: 2008, description: "First Movie of Ironman" },
-        { id: 2, name: "Ironman 2", year: 2010, description: "Second Movie of Ironman" },
-        { id: 3, name: "Ironman 3", year: 2012, description: "Third Movie of Ironman" },
+        { id: 1, name: "Ironman", image: "https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg", year: 2008, description: "First Movie of Ironman" },
+        { id: 2, name: "Ironman 2", image: "https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg", year: 2010, description: "Second Movie of Ironman" },
+        { id: 3, name: "Ironman 3", image: "https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg", year: 2012, description: "Third Movie of Ironman" },
     ]
 
     return (
         <div className='my-10 p-2'>
             <h1 className='bg-blue-200'>{heading}</h1>
-            <ul>
+            <ul className='flex flex-row'>
                 {serieses.map((series) => (
-                    <li key={series.id}>
-                        {series.name} ({series.year}) - {series.description}
+                    <li key={series.id} className='flex flex-col gap-4'>
+                        <img src={series.image} alt="photo" />
+                        <li>{series.name}</li>
+                        <li>{series.year}</li>
+                        <li>{series.description}</li>
                     </li>
                 ))}
             </ul>
